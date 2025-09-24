251f13c0266d13b9d349e218a5c68376c8059f6e
nathanpee2006 <nathanpee2006@gmail.com>
2025-09-25 10:30:20 +1200

    Configure CORS

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ALLOWED_ORIGIN,
+  })
+);
 app.use(express.json());
 
 // Routes
